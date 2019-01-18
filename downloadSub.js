937fa6200214007678552b55fbb19d055147fb60
Lilith <lily@l1lith.com>
2019-01-17 23:26:03 -0700

    add customizable filename

--- downloadSub.js
+++ downloadSub.js
@@ -1,17 +1,18 @@
 const fetch = require('./fetch')
 const {JSDOM} = require('jsdom')
 const request = require('request')
-const {join} = require('path')
+const {join, basename} = require('path')
 const {createWriteStream} = require('fs')
+const mkdirpAsync = require('./mkdirpAsync')
 
-async function downloadSub(subData, downloadFolder) {
+async function downloadSub(subData, downloadFolder, fileName) {
   const rawHTML = (await fetch(subData.url)).body.toString()
   const subDocument = (new JSDOM(rawHTML)).window.document
   const downloadURL = "https://subscene.com" + subDocument.getElementById('downloadButton').href
 
   const {body} = await fetch(downloadURL)
 
-  const stream = createWriteStream(join(downloadFolder, subData.title + '.zip'))
+  const stream = createWriteStream(join(downloadFolder, (basename(fileName || "") || subData.title) + '.zip'))
   stream.write(body)
   stream.close()
 }
