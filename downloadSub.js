a5f9c71a7e682808e294fde9bbcded80c3ef6ed4
Lilith <lily@l1lith.com>
2019-01-17 23:36:42 -0700

    get downloadSub working

--- downloadSub.js
+++ downloadSub.js
@@ -1,9 +1,10 @@
 const fetch = require('./fetch')
 const {JSDOM} = require('jsdom')
 const request = require('request')
-const {join, basename} = require('path')
+const {join, basename, extname} = require('path')
 const {createWriteStream} = require('fs')
 const mkdirpAsync = require('./mkdirpAsync')
+const AdmZip = require('adm-zip')
 
 async function downloadSub(subData, downloadFolder, fileName) {
   const rawHTML = (await fetch(subData.url)).body.toString()
@@ -12,8 +13,12 @@ async function downloadSub(subData, downloadFolder, fileName) {
 
   const {body} = await fetch(downloadURL)
 
-  const stream = createWriteStream(join(downloadFolder, (basename(fileName || "") || subData.title) + '.zip'))
-  stream.write(body)
+  const zip = new AdmZip(body)
+  const subEntry = zip.getEntries()[0]
+  const subBuffer = subEntry.getData()
+  const subDestination = join(downloadFolder, (basename(fileName || "") || subData.title) + extname(subEntry.entryName))
+  const stream = createWriteStream(subDestination)
+  stream.write(subBuffer)
   stream.close()
 }
 
